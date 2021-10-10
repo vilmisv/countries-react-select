6fbb5876b2f7e3ae1a5b5b48a68175ead53fbb54
Vilmantas <75268132+vilmisv@users.noreply.github.com>
2021-10-10 21:57:51 +0300

    countries react select

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
+import Select from 'react-select'
 import './App.css';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const countriesData = await fetchCountries()
+      setCountries(countriesData)
+    }
+      getData()
+  }, [])
+
+  const fetchCountries = async () => {
+    const res = await fetch('https://vilmisv.github.io/jsonApiCountries/myData.json');
+    const data = await res.json()
+    console.log(data)
+    return data
+  }
+
+  const options = countries.map((country) =>({
+    "value": country.country,
+    "label": country.country
+  }))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Select options={options}/>
     </div>
   );
 }
